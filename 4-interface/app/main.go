0d9fbb32114a8072d8d6b16bf804f0d5d953d6ea
ramamimu <ahm.f.ramadhani@gmail.com>
2024-08-09 10:37:32 +0700

    feat: prometheus and grafana monitoring

--- 4-interface/app/main.go
+++ 4-interface/app/main.go
@@ -7,6 +7,7 @@ import (
 	"math/rand/v2"
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -15,7 +16,20 @@ type Person struct {
 	Age  int    `json:"age"`
 }
 
+var (
+	opsProcessed = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name:      "response_variance",
+			Namespace: "http_request_count",
+		},
+		[]string{"method", "path", "code"},
+	)
+)
+
 func main() {
+	prometheus.MustRegister(opsProcessed)
+
+	// pull method
 	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
 		io.WriteString(w, "pong")
@@ -38,18 +52,24 @@ func main() {
 		fmt.Println("got ", randomizer)
 		switch randomizer {
 		case 0:
+			opsProcessed.WithLabelValues("GET", "/monitor", "405").Inc()
 			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 		case 1:
+			opsProcessed.WithLabelValues("GET", "/monitor", "500").Inc()
 			http.Error(w, "internal server error", http.StatusInternalServerError)
 		case 2:
+			opsProcessed.WithLabelValues("GET", "/monitor", "400").Inc()
 			http.Error(w, "bad request", http.StatusBadRequest)
 		case 3:
+			opsProcessed.WithLabelValues("GET", "/monitor", "201").Inc()
 			w.WriteHeader(http.StatusCreated)
 			io.WriteString(w, "success create account")
 		case 4:
+			opsProcessed.WithLabelValues("GET", "/monitor", "200").Inc()
 			w.WriteHeader(http.StatusOK)
 			io.WriteString(w, "success fetch the data")
 		default:
+			opsProcessed.WithLabelValues("GET", "/monitor", "200").Inc()
 			w.WriteHeader(http.StatusOK)
 			io.WriteString(w, "success fetch the data")
 		}
